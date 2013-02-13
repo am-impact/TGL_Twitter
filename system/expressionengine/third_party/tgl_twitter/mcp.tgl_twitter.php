65bdf324b899e4b7ede51f16a81fea4d23565d49
R.vanzon <r.vanzon@am-impact.nl>
2013-02-13 14:51:13 +0100

    Add admin configuration controller and submit

--- system/expressionengine/third_party/tgl_twitter/mcp.tgl_twitter.php
+++ system/expressionengine/third_party/tgl_twitter/mcp.tgl_twitter.php
@@ -66,47 +66,19 @@ class Tgl_twitter_mcp
 	 * @return void
 	 * @author Bryant Hughes
 	 */
-	public function submit_settings()
+	public function configuration()
 	{
-		
-		$this->EE->load->model('tgl_twitter_model');
-		
-		//loops through the post and adds all settings (deletes old settings first)
-		$success = $this->EE->tgl_twitter_model->insert_new_settings();
-		
-		$settings = $this->EE->tgl_twitter_model->get_settings();
-		
-		if($success && isset($settings['pin']) && ! isset($settings['access_token'], $settings['access_token_secret'])){
-			
-			//if a pin has been submitted, we want to generate the access tokens for the app
-			if($this->generate_access_tokens($settings)){
-				$this->EE->session->set_flashdata('message_success', $this->EE->lang->line('Success! You are now Authenticated.'));
-			}else{
-				
-				//if the pin was not able to be created, delete the submitted pin and send the user back to the authenticate page.
-				/*
-					TODO : we could use some better UX here.  Ideally sending the user back to this page happens after they create a request token, 
-								 and sending them back because of an invaid acess token authentication is somewhat confusing
-				*/
-				$this->EE->tgl_twitter_model->delete_setting('pin');
-				$this->EE->session->set_flashdata('message_failure', $this->EE->lang->line('Error authenticating with Twitter. Please verify Pin and re-submit'));
-				$this->EE->functions->redirect(BASE.AMP.'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module=tgl_twitter'.AMP.'method=register_with_twitter');
-			}
-			
-		}else{
-			
-			//else : sumission before pin as been submitted, or after all settings have been submitted
-			
-			if(! $success){
-			  $this->EE->session->set_flashdata('message_failure', $this->EE->lang->line('Error saving settings.'));
-			}else{
-			  $this->EE->session->set_flashdata('message_success', $this->EE->lang->line('Success!'));
-			}
-			
+		if ($this->EE->session->userdata('group_id') != 1)
+		{
+			$this->EE->functions->redirect(BASE . AMP . 'C=addons_modules' . AMP . 'M=show_module_cp' . AMP . 'module=tgl_twitter');
 		}
-		
-		$this->EE->functions->redirect(BASE.AMP.'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module=tgl_twitter');
 
+		$this->EE->load->model('tgl_twitter_model');
+
+		$this->data['form_action'] = AMP . 'C=addons_modules' . AMP . 'M=show_module_cp' . AMP . 'module=tgl_twitter' . AMP . 'method=submit_configuration';
+		$this->data['settings']    = $this->EE->tgl_twitter_model->get_settings();
+
+		return $this->EE->load->view('configuration', $this->data, TRUE);
 	}
 
 	/**
