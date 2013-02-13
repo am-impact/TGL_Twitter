c00277f266e91f2762e8ea594ab7ff7d0b1ce110
R.vanzon <r.vanzon@am-impact.nl>
2013-02-13 14:59:02 +0100

    Clean up of Tgl twitter install function

--- system/expressionengine/third_party/tgl_twitter/upd.tgl_twitter.php
+++ system/expressionengine/third_party/tgl_twitter/upd.tgl_twitter.php
@@ -22,26 +22,18 @@ class Tgl_twitter_upd
 
 		//create tgl_twitter module settings table
 		$fields = array(
-			'id'		=>	array('type' => 'int', 'constraint' => '10', 'unsigned' => TRUE, 'null' => FALSE, 'auto_increment' => TRUE),
-			'site_id'	=>	array('type' => 'int', 'constraint' => '8', 'unsigned' => TRUE, 'null' => FALSE, 'default' => '1'),
-			'var'		=>	array('type' => 'varchar', 'constraint' => '60', 'null' => FALSE),
-			'var_value'	=>	array('type' => 'varchar', 'constraint' => '100', 'null' => FALSE)
+			'id'                 => array('type' => 'int', 'constraint' => '10', 'unsigned' => TRUE, 'null' => FALSE, 'auto_increment' => TRUE),
+			'site_id'            => array('type' => 'int', 'constraint' => '8', 'unsigned' => TRUE, 'null' => FALSE, 'default' => '1'),
+			'consumer_key'       => array('type' => 'varchar', 'constraint' => '100', 'null' => TRUE),
+			'consumer_secret'    => array('type' => 'varchar', 'constraint' => '100', 'null' => TRUE),
+			'oauth_token'        => array('type' => 'varchar', 'constraint' => '100', 'null' => TRUE),
+			'oauth_token_secret' => array('type' => 'varchar', 'constraint' => '100', 'null' => TRUE)
 		);
 
 		$this->EE->dbforge->add_field($fields);
 		$this->EE->dbforge->add_key('id', TRUE);
 		$this->EE->dbforge->create_table('tgl_twitter_settings');
-		
-		// get the module id
-		$results = $this->EE->db->query("SELECT * FROM exp_modules WHERE module_name = 'TGL Twitter'");
-		$module_id = $results->row('module_id');
-			
-		$sql = array();
-		$sql[] = 
-					"INSERT IGNORE INTO exp_tgl_twitter_settings ".
-					"(id, site_id, var, var_value) VALUES ".
-					"('', '0', 'module_id', " . $module_id . ")";
-					
+
 		return TRUE;
 	}
 
